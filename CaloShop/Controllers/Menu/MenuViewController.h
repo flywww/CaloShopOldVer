f3febaa059e1439c776d351b02bdab3c39ce66f2
stanley <flywww004@gmail.com>
2014-05-15 20:00:33 +0800

    Basic Circle View done

---
+++ CaloShop/Controllers/Menu/MenuViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.h
+//  CaloShop
+//
+//  Created by 林盈志 on 2014/4/29.
+//  Copyright (c) 2014年 林盈志. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface MenuViewController : ViewController
+
+@property (nonatomic, weak) MSDynamicsDrawerViewController *dynamicsDrawerViewController;
+
+@end
---
+++ CaloShop/Controllers/Store/BuyInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuyInfoViewController.h
+//  CaloShop
+//
+//  Created by 林盈志 on 2014/5/12.
+//  Copyright (c) 2014年 林盈志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuyInfoViewController : UIViewController
+
+@end
