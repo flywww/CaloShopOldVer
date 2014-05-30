11f8893c4d6878b9de2362c798f2339a1112a745
stanley <flywww004@gmail.com>
2014-05-30 15:39:50 +0800

    Gesture done

---
+++ CaloShop/Controllers/Main/testView.h
@@ -0,0 +1,13 @@
+//
+//  testView.h
+//  CaloShop
+//
+//  Created by 林盈志 on 2014/5/30.
+//  Copyright (c) 2014年 林盈志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testView : UIView
+
+@end
---
+++ CaloShop/Controllers/Main/testView.m
@@ -0,0 +1,31 @@
+//
+//  testView.m
+//  CaloShop
+//
+//  Created by 林盈志 on 2014/5/30.
+//  Copyright (c) 2014年 林盈志. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
